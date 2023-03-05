6fcb249d708a3871eb9ec11cfe83118d5ab02e93
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-03-05 16:00:50 +1030

    added add a deparment/role function, added add/delete an employee function

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ async function start() {
         'Add a department',
         'Add a role',
         'Add an employee',
+        'Delete an employee',
         'Update an employee role'
       ]
     });
@@ -56,6 +57,9 @@ async function start() {
         break;
       case 'Add an employee':
         addEmployee();
+        break;
+      case 'Delete an employee':
+        deleteEmployee();
         break;
       case 'Update an employee role':
         updateEmployeeRole();
@@ -104,6 +108,147 @@ async function start() {
     mainMenu();
   }
   
+  //add Department function
+  async function addDepartment() {
+    const { name } = await inquirer.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the department?'
+    });
+    await connection.execute(`
+      INSERT INTO department (name)
+      VALUES (?)
+    `, [name])
+    .catch(error => console.log(error));
+    console.log(`Added ${name} to the database`);
+    mainMenu();
+  }
+
+  //add Role function
+  async function addRole() {
+    const departments = await connection.execute(`
+      SELECT * FROM department
+    `)
+    .catch(error => console.log(error));
+    const departmentChoices = departments[0].map(({ id, name }) => ({
+      name: name,
+      value: id
+    }));
+    const role = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of the role?'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary of the role?'
+      },
+      {
+        type: 'list',
+        name: 'department_id',
+        message: 'Which department does the role belong to?',
+        choices: departmentChoices
+      }
+    ]);
+    await connection.execute(`
+      INSERT INTO role (title, salary, department_id)
+      VALUES (?, ?, ?)
+    `, [role.title, role.salary, role.department_id])
+    .catch(error => console.log(error));
+    console.log(`Added ${role.title} to the database`);
+    mainMenu();
+  }
+  
+  //add Employee function
+  async function addEmployee() {
+    const roles = await connection.execute(`
+      SELECT * FROM role
+    `)
+    .catch(error => console.log(error));
+    const roleChoices = roles[0].map(({ id, title }) => ({
+      name: title,
+      value: id
+    }));
+    const employees = await connection.execute(`
+      SELECT * FROM employee
+    `)
+    .catch(error => console.log(error));
+    const employeeChoices = employees[0].map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+    employeeChoices.unshift({ name: 'None', value: null });
+    const employee = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: "What is the employee's first name?"
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: "What is the employee's last name?"
+      },
+      {
+        type: 'list',
+        name: 'role_id',
+        message: "What is the employee's role?",
+        choices: roleChoices
+      },
+      {
+        type: 'list',
+        name: 'manager_id',
+        message: "Who is the employee's manager?",
+        choices: employeeChoices
+      }
+    ]);
+    await connection.execute(`
+      INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES (?, ?, ?, ?)
+    `, [employee.first_name, employee.last_name, employee.role_id, employee.manager_id])
+    .catch(error => console.log(error));
+    console.log(`Added ${employee.first_name} ${employee.last_name} to the database`);
+    mainMenu();
+  }
+  
+  //delete Employee function
+  async function deleteEmployee() {
+    const employees = await connection.execute(`
+      SELECT * FROM employee
+    `).catch(error => console.log(error));
+  
+    const employeeChoices = employees[0].map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id
+    }));
+  
+    const employee = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'id',
+        message: 'Which employee would you like to delete?',
+        choices: employeeChoices
+      },
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Are you sure you want to delete this employee?',
+        default: false
+      }
+    ]);
+  
+    if (employee.confirm) {
+      await connection.execute(`
+        DELETE FROM employee WHERE id = ?
+      `, [employee.id]).catch(error => console.log(error));
+      console.log(`Employee with ID ${employee.id} has been deleted`);
+    } else {
+      console.log('Deletion cancelled');
+    } 
+    mainMenu();
+  } 
 }
 
 // call the start function to start the program
