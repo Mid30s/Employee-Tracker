1bb5edc74a501fd4167f600438283588d6fd5ebd
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-03-05 14:48:07 +1030

    added banner title and updated view all department/roles/employees function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Employee-Tracker
+
+
+
+
+Using Promise Wrapper
+https://www.npmjs.com/package/mysql2#using-promise-wrapper
--- server.js
+++ server.js
@@ -1,58 +1,110 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
+const figlet = require('figlet');
 
-// create the database connection
-const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'company_db'
-});
-
-// prompt the user with the main menu
-async function mainMenu() {
-  const { choice } = await inquirer.prompt({
-    type: 'list',
-    name: 'choice',
-    message: 'What would you like to do?',
-    choices: [
-      'View all departments',
-      'View all roles',
-      'View all employees',
-      'Add a department',
-      'Add a role',
-      'Add an employee',
-      'Update an employee role'
-    ]
+async function start() {
+    //top banner using figlet
+    figlet("Employee Tracker", function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+    });
+    
+    const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'gary282566',
+    database: 'company_db'
   });
 
-  // call the appropriate function based on the user's choice
-  switch (choice) {
-    case 'View all departments':
-      viewAllDepartments();
-      break;
-    case 'View all roles':
-      viewAllRoles();
-      break;
-    case 'View all employees':
-      viewAllEmployees();
-      break;
-    case 'Add a department':
-      addDepartment();
-      break;
-    case 'Add a role':
-      addRole();
-      break;
-    case 'Add an employee':
-      addEmployee();
-      break;
-    case 'Update an employee role':
-      updateEmployeeRole();
-      break;
-    default:
-      console.log('Invalid choice');
+  // prompt the user with the main menu
+  async function mainMenu() {
+    const { choice } = await inquirer.prompt({
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'
+      ]
+    });
+
+    // call the appropriate function based on the user's choice
+    switch (choice) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      default:
+        console.log('Invalid choice');
+    }
   }
+
+  // start the program by calling the main menu
+  mainMenu();
+
+  // view All Departments function
+  async function viewAllDepartments() {
+    const [rows, fields] = await connection.execute(`
+      SELECT * FROM department
+    `)
+    .catch(error => console.log(error));
+    console.table(rows);
+    mainMenu();
+  }
+
+  //view All Roles function
+  async function viewAllRoles() {
+    const [rows, fields] = await connection.execute(`
+      SELECT role.id, role.title, department.name as department, role.salary
+      FROM role
+      LEFT JOIN department ON role.department_id = department.id
+    `)
+    .catch(error => console.log(error));
+    console.table(rows);
+    mainMenu();
+  }
+  
+  //view All Employees function
+  async function viewAllEmployees() {
+    const [rows, fields] = await connection.execute(`
+      SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) as manager
+      FROM employee
+      LEFT JOIN role ON employee.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id
+      LEFT JOIN employee manager ON manager.id = employee.manager_id
+    `)
+    .catch(error => console.log(error));
+    console.table(rows);
+    mainMenu();
+  }
+  
 }
 
-// start the program by calling the main menu
-mainMenu();
+// call the start function to start the program
+start(); 
